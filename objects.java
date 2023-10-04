d97338e50862658f50841f0530d1a2f82a1913d5
Vinit <vinitsk99@gmail.com>
2023-10-04 14:37:57 +0530

    Objects in java

---
+++ objects.java
@@ -0,0 +1,11 @@
+class one{
+        int x=20;
+}
+
+class two{
+    public static void main(String st[]){
+        //Object of class one
+        one ob1 = new one();
+        System.out.print("The value of x is: "+ob1.x);
+    }
+}

38cc5076d51a2e6ce09c5d643bd0ab0a5e11a5c0
Vinit <vinitsk99@gmail.com>
2023-10-08 22:30:39 +0530

    Addition of two numbers

---
+++ addition.java
@@ -0,0 +1,7 @@
+class Sum_cal{
+    public static void main(String st[]){
+        int a = 10, b=20;
+        int sum = a + b;
+        System.out.print("The sum of "+a+" and "+b+" is: "+sum);
+    }
+}

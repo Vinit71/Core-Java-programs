1f53c77cc9dc00916ad69bd66dcf7a7082db002f
Vinit <vinitsk99@gmail.com>
2023-10-11 15:33:23 +0530

    Method Over-riding

---
+++ overriding.java
@@ -0,0 +1,21 @@
+class Animal{
+    void sound(){
+        System.out.println("Animal makes different sounds");
+    }
+}
+
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+
+class Main{
+    public static void main(String st[]){
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.sound();
+        dog.sound();
+    }
+}

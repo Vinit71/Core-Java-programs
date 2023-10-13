dcf6e160baefca5cc45d92b50bd888534a682b24
Vinit <vinitsk99@gmail.com>
2023-10-13 22:25:41 +0530

    Final keyword in java

---
+++ final_keyword.java
@@ -0,0 +1,14 @@
+final class Final_example{
+
+    final int i = 10; //constant
+    
+    final void printable(){ 
+        System.out.print("This is a final Method, means it cannot be overriden");
+    }
+
+}
+/*FINAL KEYWORD
+1.final Variable: The variable's value cannot be changed after initialization.
+2.final Method: Method cannot be overriden by subclass
+3.final class: The class cannot be subclassed.
+ */

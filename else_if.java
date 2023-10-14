e47cf4261a1421721d24ce1a23874972d68d8e01
Vinit <vinitsk99@gmail.com>
2023-10-14 16:26:27 +0530

    else_if statement

---
+++ else_if.java
@@ -0,0 +1,17 @@
+class Grade{
+    public static void main(String st[]) {
+        int score = 75; 
+
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else if (score >= 70) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: D");
+        }
+    }
+}
+
+//It is used to test multiple conditions sequentially after an initial 'if' statement. 

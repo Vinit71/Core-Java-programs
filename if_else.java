5ac8a5dbe481aa99c743a19cfac3d48f694f997e
Vinit <vinitsk99@gmail.com>
2023-10-14 16:10:36 +0530

    If else statement

---
+++ if_else.java
@@ -0,0 +1,18 @@
+class DrivingLicense{
+    public static void main(String[] args) {
+        int age = 20;
+
+        // Check if the person is eligible for driving
+        if (age > 18 && age < 70) {
+            System.out.println("You are eligible for driving.");
+        } else {
+            System.out.println("You are not eligible for driving.");
+        }
+    }
+}
+
+/*
+ The if-else statement is used for conditional execution in programming. 
+ It allows you to specify a block of code that will be executed only if a specified condition is true. 
+ If the condition is false, an alternate block of code can be executed.
+ */

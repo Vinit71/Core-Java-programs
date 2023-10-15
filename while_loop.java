8ae327388fa47fbec834321548fd5eb0f1d5c303
Vinit <vinitsk99@gmail.com>
2023-10-15 23:23:22 +0530

    Printing Numbers

---
+++ while_loop.java
@@ -0,0 +1,15 @@
+class Numbers{
+        public static void main(String st[]) {
+        int number = 1;
+
+        System.out.println("Numbers from 1 to 5:");
+        while (number <= 5) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
+
+/*
+ * It allows the program to keep looping until a specific condition is met, making it suitable for tasks where the termination point is not predefined.
+ */

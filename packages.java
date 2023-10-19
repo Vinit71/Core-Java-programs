ee53ac1bae6d3eaa9bfc34e7de4650d42d0c2c41
Vinit <vinitsk99@gmail.com>
2023-10-19 12:00:57 +0530

    Creating Package

---
+++ packages.java
@@ -0,0 +1,21 @@
+package p1;
+
+public class packages {
+   public void display(){
+    System.out.print("Hello this function is present within package p1");
+   }
+}
+
+/*
+ * Packages are collection of classes, interfaces and subpackages.
+ * Packages can help to prevent name collisions.
+ * There are two types of packages in java: 
+    1. Build-in packages
+    2. User-defined packages
+
+ * To create a package we use keyword 'package' followed by package_name.
+    For example: package students;
+
+ * The command javac -d . file_name.java is used to compile a Java source file
+   and specify the destination directory for the compiled bytecode files. 
+ */

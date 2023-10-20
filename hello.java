67db23435f0a6ecc6d404ec2b3df824ecb71a35a
Vinit <vinitsk99@gmail.com>
2023-10-20 23:23:43 +0530

    Add comments

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
-class A{
+class FirstHello{
     public static void main(String st[]){
         System.out.print("Hello");
     }
 }
+
+/* System.out.print() is a statement in Java that prints text in the Java console. 
+ * The print() method is used to display text or other data on the console without moving the cursor to the next line.
+ * The println() method is used to display text or other data on the console and moves the cursor to the next line after printing.
+*/
